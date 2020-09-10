6d65da6405d25478c4d9a7018e7aef3ed9f21471
ggmartins <ggmartins@gmail.com>
2020-09-10 16:43:26 -0500

    feat: ouimac + rdns

--- gmartins/iotlab_destinations/iotdst.py
+++ gmartins/iotlab_destinations/iotdst.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import pandas as pd
 import pickle
+import dns.resolver,dns.reversename
+from mac_vendor_lookup import MacLookup
 from os import path
 import math
-print(pd.__version__)
-router = '00:04:4b:e4:08:c5'
 
+macresolver = MacLookup()
+macresolver.update_vendors()
+
+router = '00:04:4b:e4:08:c5'
 result = {}
 
 def process(f, r):
@@ -23,18 +27,38 @@ def process(f, r):
        addr = 'IPv4.SrcIP'
        direction = 'dw'
        mac = d['Ethernet.DstMAC']
+       try:
+          mac = macresolver.lookup(d['Ethernet.DstMAC'])
+       except:
+          #print("WARN: MAC {0} not found.".format(mac))
+          pass
+          mac = d['Ethernet.DstMAC']
        ip = d['IPv4.DstIP']
     else:
        addr = 'IPv4.DstIP'
        direction = 'up'
        mac = d['Ethernet.SrcMAC']
+       try:
+          mac = macresolver.lookup(d['Ethernet.SrcMAC'])
+       except:
+          #print("WARN: MAC {0} not found.".format(mac))
+          pass
+          mac = d['Ethernet.SrcMAC']
        ip = d['IPv4.SrcIP']
     if mac not in r[f]:
         r[f][mac] = {'ip': ip, 'destination': {}, 'count_destination': 0}
     if d[addr] not in r[f][mac]['destination']:
-        r[f][mac]['destination'][d[addr]] = {'dw' : 0.0, 'up' : 0.0}
-    r[f][mac]['destination'][d[addr]][direction] =\
+        r[f][mac]['destination'][d[addr]] = {'dw' : 0.0, 'up' : 0.0, 'rdns': None}
+    r[f][mac]['destination'][d[addr]][direction] = \
             r[f][mac]['destination'][d[addr]][direction] + size
+    if r[f][mac]['destination'][d[addr]]['rdns'] is None:
+       try:
+         r[f][mac]['destination'][d[addr]]['rdns'] = \
+            [str(a) for a in dns.resolver.resolve(dns.reversename.from_address(d[addr]),"PTR")]
+       except Exception as e:
+         #print("WARN: {0}".format(e))
+         r[f][mac]['destination'][d[addr]]['rdns'] = d[addr]
+         pass
   for k in r[f].keys():
      r[f][k]['count_destination'] = len(r[f][k]['destination'].keys())
 
